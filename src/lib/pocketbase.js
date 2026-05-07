b6cc498930fde418c467efe5707f7a81136beb4d
hamiltonmarty <marty.d.hamilton@gmail.com>
2026-05-07 09:13:33 -0500

    chore: Update pocketbase URL for dynamic hosting

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase';
 
 // The "export" keyword here is the handshake for the rest of your app
-export const pb = new PocketBase('http://family-hub.local:8090');
+export const pb = new PocketBase('/');
